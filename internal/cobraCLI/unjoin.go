51648e03ab79652729c182cf0986a930ac5f6eb0
Keith <73649950+Cyrof@users.noreply.github.com>
2025-10-06 06:34:03 +0000

    refactor(cobraCLI/unjoin): Added a confirmation before running the unjoin domain script.

--- internal/cobraCLI/unjoin.go
+++ internal/cobraCLI/unjoin.go
@@ -1,22 +1,40 @@
 package cobracli
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"path/filepath"
+	"strings"
+
 	"github.com/Cyrof/machina/internal/run"
-	
+
 	"github.com/spf13/cobra"
 )
 
 var workgroup string
 
 var unjoinCmd = &cobra.Command{
-	Use: "unjoin",
+	Use:   "unjoin",
 	Short: "Unjoin domain and join a workgroup",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if workgroup == "" {
 			workgroup = "WORKGROUP"
 		}
+
+		fmt.Println("[!] WARNING: This will unjoin the machine from the domain.")
+		fmt.Println("[!] Make sure you can log in with a local Administrator account, otherwise you may loss access.")
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Type YES to continue: ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(strings.ToUpper(input))
+
+		if input != "YES" {
+			fmt.Println("Operation cancelled.")
+			return nil
+		}
+
 		script := filepath.Join("scripts", "unjoin_to_workgroup.bat")
 		fmt.Printf("[*] Running unjoin -> workgroup (%s)\n", workgroup)
 		return run.BAT(script, workgroup)
